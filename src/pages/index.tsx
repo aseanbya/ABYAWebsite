39f9a6dde95acd2f7d26f341dc0d8306fdb67692
javianng <j4vianz01@gmail.com>
2023-07-07 02:35:47 +0800

    feat: implement navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from './Navbar';
+
 export default function Home() {
   return (
-    <>
-      <div>
-        apple
-      </div>
-    </>
+    <ChakraProvider>
+      <Navbar />
+    </ChakraProvider>
   );
 }
