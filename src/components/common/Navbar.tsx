38ac9295e07fe28768a1e319b12b659bb1425aee
javianng <j4vianz01@gmail.com>
2023-08-09 19:52:35 +0800

    fix: updated a to next link

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import logo from 'src/assets/logo.png'
 
 export default function Navbar() {
@@ -12,30 +13,30 @@ export default function Navbar() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a href="/"> Home </a></li>
+                        <li><Link href="/"> Home </Link></li>
                         <li>
                             <a>Get Involved</a>
                             <ul className="p-2">
-                                <li><a href="/getInvolved/partner"> Partner </a></li>
-                                <li><a href="/getInvolved/executive"> Executive </a></li>
-                                <li><a href="/getInvolved/member"> Member </a></li>
+                                <li><Link href="/getInvolved/partner"> Partner </Link></li>
+                                <li><Link href="/getInvolved/executive"> Executive </Link></li>
+                                <li><Link href="/getInvolved/member"> Member </Link></li>
                             </ul>
                         </li>
                         <li>
                             <a>Our Events</a>
                             <ul className="p-2">
-                                <li><a href="/event/ABYAExplore"> ABYA Explore </a></li>
-                                <li><a href="/event/ABYALeaps"> ABYA Leaps </a></li>
+                                <li><Link href="/event/ABYAExplore"> ABYA Explore </Link></li>
+                                <li><Link href="/event/ABYALeaps"> ABYA Leaps </Link></li>
                             </ul>
                         </li>
                         <li>
                             <a>About Us</a>
                             <ul className="p-2">
-                                <li><a href="/aboutUs/ABYAHistory"> ABYA History </a></li>
-                                <li><a href="/aboutUs/ourImpact"> Our Impact </a></li>
+                                <li><Link href="/aboutUs/ABYAHistory"> ABYA History </Link></li>
+                                <li><Link href="/aboutUs/ourImpact"> Our Impact </Link></li>
                             </ul>
                         </li>
-                        <li><a href="/ourSupporters"> Our Supporters </a></li>
+                        <li><Link href="/ourSupporters"> Our Supporters </Link></li>
                     </ul>
                 </div>
                 <a className="btn btn-ghost normal-case text-xl hidden lg:flex">
@@ -58,14 +59,14 @@ export default function Navbar() {
             <div className="navbar-end hidden lg:flex">
 
                 <ul className="menu menu-horizontal px-1">
-                    <li><a href="/"> Home </a></li>
+                    <li><Link href="/"> Home </Link></li>
                     <li tabIndex={0}>
                         <details>
                             <summary> Get Involved </summary>
                             <ul className="p-2">
-                                <li><a href="/getInvolved/partner"> Partner </a></li>
-                                <li><a href="/getInvolved/executive"> Executive </a></li>
-                                <li><a href="/getInvolved/member"> Member </a></li>
+                                <li><Link href="/getInvolved/partner"> Partner </Link></li>
+                                <li><Link href="/getInvolved/executive"> Executive </Link></li>
+                                <li><Link href="/getInvolved/member"> Member </Link></li>
                             </ul>
                         </details>
                     </li>
@@ -73,8 +74,8 @@ export default function Navbar() {
                         <details>
                             <summary> Our Events </summary>
                             <ul className="p-2">
-                                <li><a href="/event/ABYAExplore"> ABYA Explore </a></li>
-                                <li><a href="/event/ABYALeaps"> ABYA Leaps </a></li>
+                                <li><Link href="/event/ABYAExplore"> ABYA Explore </Link></li>
+                                <li><Link href="/event/ABYALeaps"> ABYA Leaps </Link></li>
                             </ul>
                         </details>
                     </li>
@@ -82,12 +83,12 @@ export default function Navbar() {
                         <details>
                             <summary> About Us </summary>
                             <ul className="p-2">
-                                <li><a href="/aboutUs/ABYAHistory"> ABYA History </a></li>
-                                <li><a href="/aboutUs/ourImpact"> Our Impact </a></li>
+                                <li><Link href="/aboutUs/ABYAHistory"> ABYA History </Link></li>
+                                <li><Link href="/aboutUs/ourImpact"> Our Impact </Link></li>
                             </ul>
                         </details>
                     </li>
-                    <li><a href="/ourSupporters"> Our Supporters </a></li>
+                    <li><Link href="/ourSupporters"> Our Supporters </Link></li>
                 </ul>
             </div>
         </div>
