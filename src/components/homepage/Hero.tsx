b458eb78da1ec5a0fa916c7f61934a4d08aab620
javianng <j4vianz01@gmail.com>
2023-08-11 20:27:37 +0800

    feat: added image to hero

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image"
+import heroImage from "src/assets/heroImage.png"
+
 function GetInvolvedButton() {
     return (
         <button
@@ -15,10 +18,17 @@ export default function Hero() {
             <div className="hero-content text-start">
                 <div className="p-5">
                     <h1 className="text-3xl font-bold uppercase tracking-wide">Shaping the future</h1>
-                    <div className="w-3/5">
-                        <p className="py-4 text-sm">We believe in supporting Youths in ASEAN as we navigate through diverse cultures and the future of business opportunity</p>
+                    <div className="flex flex-row">
+                        <div>
+                            <p className="py-4 text-sm">We believe in supporting Youths in ASEAN as we navigate through diverse cultures and the future of business opportunity</p>
+                            <GetInvolvedButton />
+                        </div>
+                        <Image
+                            src={heroImage}
+                            alt={""}
+                            className="w-1/2"
+                        />
                     </div>
-                    <GetInvolvedButton />
                 </div>
             </div>
         </div>
