398031cd88355090def3946131280efb8d5cb4d7
p-xp <99188356+p-xp@users.noreply.github.com>
2023-08-16 23:02:12 +0800

    feat: Add image background

--- src/pages/getInvolved/index.tsx
+++ src/pages/getInvolved/index.tsx
@@ -11,7 +11,6 @@ export default function getInvolved() {
                     "Amet ex ipsum adipisicing. Consectetur veniam officia pariatur officia in et dolor ex sint consectetur. Dolor aliqua ipsum et labore est. Enim ea tempor quis sint eu non commodo non id anim id non laborum labore. Veniam consequat pariatur duis. Dolor ut laborum in et esse anim commodo eu minim pariatur officia pariatur sit non. Sit aliquip anim sunt eiusmod proident labore Lorem est sit aliqua sunt minim minim occaecat ut."
                 }
             />
-            <OurCommunity />
         </PageLayout>
     );
 }
