a944116b49e347c193a5958fdc919b281480854d
javianng <j4vianz01@gmail.com>
2023-08-16 09:52:14 +0800

    feat: added header to page

--- src/pages/aboutUs/index.tsx
+++ src/pages/aboutUs/index.tsx
@@ -1,10 +1,14 @@
 import PageLayout from "~/components/common/PageLayout";
+import PageTitleSection from "~/components/common/PageTitleSection";
 
 
 export default function aboutUs() {
     return (
         <PageLayout>
-            aboutUs
+            <PageTitleSection
+                title="About Us"
+                subtitle="Aliquip tempor culpa nulla aliqua ut in commodo non esse mollit quis adipisicing. Ipsum dolor duis pariatur ad Lorem aliquip ex minim aliquip do pariatur ut. Ad sint minim duis mollit culpa. Ea deserunt id ex esse veniam anim et deserunt commodo consectetur laborum id. Reprehenderit exercitation qui dolor magna consectetur proident aliqua nostrud eu aliquip. In consequat amet et deserunt do irure velit tempor ipsum occaecat."
+            />
         </PageLayout>
     );
 }
