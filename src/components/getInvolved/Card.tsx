cdf4f2c4c70f3ddf120f3a92b0464443196f8897
sheng <chengzhisheng2001@gmail.com>
2023-08-20 10:45:21 +0800

    fix: card sizing issues

--- src/components/getInvolved/Card.tsx
+++ src/components/getInvolved/Card.tsx
@@ -8,8 +8,8 @@ type CardProps = {
 // TODO: Add image to Card
 export const Card: React.FC<CardProps> = ({ title, description, href }) => {
     return (
-        <div className="card w-full border border-blue-500 bg-base-100 px-4 pt-6 shadow-xl lg:w-[428px] lg:px-7 lg:pt-8">
-            <figure className="h-[270px] w-full rounded-xl bg-gray-300 lg:w-[360px]"></figure>
+        <div className="card w-full border border-brandBlue bg-base-100 px-4 pt-6 shadow-xl lg:px-7 lg:pt-8">
+            <figure className="aspect-[4/3] w-full rounded-xl bg-gray-300"></figure>
             <div className="card-body px-0">
                 <h2 className="card-title text-2xl font-bold uppercase">
                     {title}
