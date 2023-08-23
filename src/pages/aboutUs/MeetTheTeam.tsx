414222638c58d85155dddd5a9118a4c58f619c07
mochicooki3 <132681043+mochicooki3@users.noreply.github.com>
2023-08-23 10:34:30 +0800

    feat: update team component with carousel feature

--- src/pages/aboutUs/MeetTheTeam.tsx
+++ src/pages/aboutUs/MeetTheTeam.tsx
@@ -1,5 +1,6 @@
 import personImage from "src/assets/person-image.jpg";
 import Image from "next/image";
+import { useState } from "react";
 
 interface Member {
     name: string;
@@ -48,6 +49,38 @@ const memberList: Record<string, Member[]> = {
             name: "Udin",
             role: "Board 3",
         },
+        {
+            name: "Nadia",
+            role: "Board 4",
+        },
+        {
+            name: "Lyssa",
+            role: "Board 5",
+        },
+        {
+            name: "Udin",
+            role: "Board 6",
+        },
+        {
+            name: "Nadia",
+            role: "Board 7",
+        },
+        {
+            name: "Lyssa",
+            role: "Board 8",
+        },
+        {
+            name: "Udin",
+            role: "Board 9",
+        },
+        {
+            name: "Nadia",
+            role: "Board 10",
+        },
+        {
+            name: "Lyssa",
+            role: "Board 11",
+        },
     ],
 };
 
@@ -59,7 +92,7 @@ interface MemberCardProps {
 function MemberCard({ member, color }: MemberCardProps) {
     return (
         <div
-            className="card flex w-96 bg-brandBlue shadow-xl"
+            className={`card mb-10 flex w-96 border-2 border-${color} shadow-xl`}
             style={{ maxHeight: "400px" }}
         >
             <figure>
@@ -73,35 +106,90 @@ function MemberCard({ member, color }: MemberCardProps) {
     );
 }
 
-/* function MemberCard({ member, color }: MemberCardProps) {
-    return (
-        <div
-            className={`border-shadow-xl border-${color} card w-96 border`}
-            style={{ maxHeight: "400px", borderWidth: "3px" }}
-        >
-            <figure>
-                <Image src={personImage} alt="Member Photo" />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">{member.name}</h2>
-                <p>{member.role}</p>
-            </div>
-        </div>
-    );
-}
-*/
-
 interface MemberGridProps {
     members: Member[];
     color: string;
+    groupName: string;
 }
 
-function MemberGrid({ members, color }: MemberGridProps) {
+function MemberGrid({ members, color, groupName }: MemberGridProps) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const slicedMembers = [];
+    for (let i = 0; i < members.length; i += 3) {
+        slicedMembers.push(members.slice(i, i + 3)); // push in an array of 3 members
+    }
+
+    const handleNextClick = () => {
+        if (currentIndex < slicedMembers.length - 1) {
+            setCurrentIndex(currentIndex + 1);
+        }
+    };
+
+    const handlePrevClick = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+        }
+    };
+
+    console.log(slicedMembers);
     return (
-        <div className="mb-10 grid grid-cols-3 gap-4">
-            {members.map((member, index) => (
-                <MemberCard key={index} member={member} color={color} />
-            ))}
+        <div>
+            <div className="flex items-center justify-center">
+                <button
+                    className="btn-outline btn-circle btn mr-10"
+                    onClick={handlePrevClick}
+                    style={{ opacity: currentIndex === 0 ? 0.25 : 1 }}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M15 19l-7-7 7-7"
+                        />
+                    </svg>
+                </button>
+                <h1 className="mb-10 mt-10 text-center text-3xl font-semibold">
+                    {groupName}
+                </h1>
+                <button
+                    className="btn-outline btn-circle btn ml-10"
+                    onClick={handleNextClick}
+                    style={{
+                        opacity:
+                            currentIndex === slicedMembers.length - 1
+                                ? 0.25
+                                : 1,
+                    }}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M9 5l7 7-7 7"
+                        />
+                    </svg>
+                </button>
+            </div>
+            <div className="grid w-full grid-cols-3 gap-10">
+                {slicedMembers[currentIndex]?.map((member, index) => (
+                    <MemberCard key={index} member={member} color={color} />
+                ))}
+            </div>
         </div>
     );
 }
@@ -114,17 +202,23 @@ export default function MeetTheTeam() {
                     Meet The Team
                 </h1>
             </div>
-            {Object.entries(memberList).map(([groupName, members]) => (
-                <div
-                    className="ml-10 mt-5 flex-col justify-center"
-                    key={groupName}
-                >
-                    <h1 className="mb-10 text-center text-xl font-semibold">
-                        {groupName}
-                    </h1>
-                    <MemberGrid members={members} color={"brandBlue"} />
-                </div>
-            ))}
+            <div className="flex min-h-screen flex-col items-center justify-center">
+                {Object.entries(memberList).map(([groupName, members]) => (
+                    <div key={groupName}>
+                        <MemberGrid
+                            members={members}
+                            color={
+                                groupName === "Management Committee"
+                                    ? "brandBlue"
+                                    : groupName === "Executives"
+                                    ? "brandYellow"
+                                    : "brandRed"
+                            }
+                            groupName={groupName}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
