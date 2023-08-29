615c04b614ad6f794db8181dc0ce2b0ccae8cb31
javianng <j4vianz01@gmail.com>
2023-08-29 23:30:09 +0800

    feat: implement dynamic blog

--- src/components/blog/IndividualBlogPage/IndividualBlogImageBody.tsx
+++ src/components/blog/IndividualBlogPage/IndividualBlogImageBody.tsx
@@ -2,18 +2,40 @@ import Image, { StaticImageData } from "next/image"
 import imag2e from "src/assets/annualImpactReport.png"
 
 type IndividualBlogImageBodyProps = {
-    title?: string,
+    text?: string,
     image?: string | StaticImageData,
+    location?: string,
 }
 
-
-export default function IndividualBlogImageBody({ title, image }: IndividualBlogImageBodyProps) {
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 items-center">
-            <h1 className="text-5xl sm:text-7xl font-bold text-black pb-2 capitalize">
-                {title ?? "Blog's Title Goes Here"}
-            </h1>
-            <Image src={image ?? imag2e} alt={""} className="" />
-        </div>
-    )
+export default function IndividualBlogImageBody({ text: text, image, location }: IndividualBlogImageBodyProps) {
+    if (location == "left" || location == "right") {
+        return (
+            <div className="grid grid-cols-1 sm:grid-cols-2 items-center gap-4">
+                {location == "right" &&
+                    <Image src={image ?? imag2e} alt={""} />
+                }
+                <h1 className="text-black pb-2">
+                    {text ?? "Ullamco non laboris aliquip deserunt est laborum Lorem. Ex ullamco voluptate laborum sint non magna duis mollit officia nulla incididunt. Est do aliqua est nisi nostrud ullamco ex adipisicing qui ipsum consequat. Mollit eiusmod sit ex elit ea proident enim est tempor exercitation exercitation. Deserunt dolor eiusmod consequat ullamco eiusmod. Sint dolor enim in officia ut enim eiusmod officia ea consectetur sunt cillum dolor reprehenderit cillum."}
+                </h1>
+                {location == "left" &&
+                    <Image src={image ?? imag2e} alt={""} />
+                }
+            </div>
+        )
+    }
+    if (location == "top" || location == "bottom") {
+        return (
+            <div className="flex flex-col items-center gap-4">
+                {location == "top" &&
+                    <Image src={image ?? imag2e} alt={""} />
+                }
+                <h1 className="text-black pb-2">
+                    {text ?? "Ullamco non laboris aliquip deserunt est laborum Lorem. Ex ullamco voluptate laborum sint non magna duis mollit officia nulla incididunt. Est do aliqua est nisi nostrud ullamco ex adipisicing qui ipsum consequat. Mollit eiusmod sit ex elit ea proident enim est tempor exercitation exercitation. Deserunt dolor eiusmod consequat ullamco eiusmod. Sint dolor enim in officia ut enim eiusmod officia ea consectetur sunt cillum dolor reprehenderit cillum."}
+                </h1>
+                {location == "bottom" &&
+                    <Image src={image ?? imag2e} alt={""} />
+                }
+            </div>
+        )
+    }
 }
--- src/pages/blog/TestBlog.tsx
+++ src/pages/blog/TestBlog.tsx
@@ -1,11 +1,11 @@
+import IndividualBlogImageBody from "~/components/blog/IndividualBlogPage/IndividualBlogImageBody";
 import BlogTemplate from "../../components/blog/IndividualBlogPage/BlogTemplate";
+import imag2e from "src/assets/annualImpactReport.png"
 
 export default function TestBlog() {
     return (
         <BlogTemplate>
-            <div>
-                dsa
-            </div>
+            <IndividualBlogImageBody image={imag2e} location="right" />
         </BlogTemplate>
     )
 }
