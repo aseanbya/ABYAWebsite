fc9a496a091b6f2aa0591cc066582eb027304759
mochicooki3 <132681043+mochicooki3@users.noreply.github.com>
2023-08-30 23:47:31 +0800

    fix: fix merge errors with main

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -6,9 +6,9 @@ function GetInvolvedButton() {
     return (
         <button
             className="bg-brandBlue text-white rounded-xl py-2 px-4
-                hover:bg-slate-700"
+            hover:bg-slate-700"
         >
-            <p className="font-bold text-xl uppercase">
+            <p className="font-bold text-md sm:text-xl uppercase">
                 Get Involved
             </p>
         </button>
@@ -20,16 +20,16 @@ export default function Hero() {
         <ContentContainer className="bg-neutral-100">
             <div className="text-start">
                 <div className="py-8">
-                    <h1 className=" text-7xl font-bold uppercase tracking-wide text-black">Shaping the future</h1>
-                    <div className="flex flex-row">
-                        <div>
-                            <p className="py-4 text-xl">We believe in supporting Youths in ASEAN as we navigate through diverse cultures and the future of business opportunity</p>
+                    <h1 className="text-5xl sm:text-7xl font-bold uppercase tracking-wide text-black">Shaping the future</h1>
+                    <div className="flex flex-col sm:flex-row">
+                        <div className="w-full sm:w-1/2">
+                            <p className="py-4 text-md sm:text-xl">We believe in supporting Youths in ASEAN as we navigate through diverse cultures and the future of business opportunity</p>
                             <GetInvolvedButton />
                         </div>
                         <Image
                             src={singaporeAttractions}
                             alt={""}
-                            className="w-1/2"
+                            className="hidden sm:flex sm:w-1/2"
                         />
                     </div>
                 </div>
