796ed3ec0a9fa637db82d768964df10e9063f5d9
p-xp <panxinping10@gmail.com>
2023-09-09 11:10:36 +0800

    Add BlogDetails file

---
+++ src/components/details/BlogDetails.tsx
@@ -0,0 +1,32 @@
+enum BlogType {
+    blog = "Blog",
+    aseanSeries = "aseanSeries"
+}
+
+export const BLOG_CONTENT: BlogContent[] = [
+    {
+        title: "Title goes here",
+        date: new Date(),
+        tag:
+    },
+    {
+        title: "Title goes here",
+        date: new Date(),
+    },
+    {
+        title: "Title goes here",
+        date: new Date(),
+    },
+    {
+        title: "Title goes here",
+        date: new Date(),
+    },
+    {
+        title: "Title goes here",
+        date: new Date(),
+    },
+    {
+        title: "Title goes here",
+        date: new Date(),
+    },
+];
