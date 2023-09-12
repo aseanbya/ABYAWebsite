46a8eef6fe1193941f4f4d5cb63e139121708b3c
mochicooki3 <132681043+mochicooki3@users.noreply.github.com>
2023-09-13 00:43:26 +0800

    fix: unable to load pdf file

--- next.config.mjs
+++ next.config.mjs
@@ -9,23 +9,41 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+    /**
+     * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+     * out.
+     *
+     * @see https://github.com/vercel/next.js/issues/41980
+     */
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
+
+    // handle pdf files
+    webpack: (config, { isServer }) => {
+        config.module.rules.push({
+            test: /\.(pdf)$/,
+            use: [
+                {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        outputPath: "static/pdf/",
+                    },
+                },
+            ],
+        });
+
+        return config;
+    },
 };
 
 const withNextra = nextra({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
+    theme: "nextra-theme-docs",
+    themeConfig: "./theme.config.tsx",
 });
 
 export default withNextra(config);
