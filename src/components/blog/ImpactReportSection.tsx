fd2392febebea07d70129896701bb869244cd04b
mochicooki3 <132681043+mochicooki3@users.noreply.github.com>
2023-09-13 00:06:44 +0800

    feat: implement pdf flipbook

--- src/components/blog/ImpactReportSection.tsx
+++ src/components/blog/ImpactReportSection.tsx
@@ -1,17 +1,36 @@
-import React from "react";
-import Image from "next/image";
-import annualImpactReportImg from "src/assets/annualImpactReport.png";
+import React, { useState, useEffect, useRef } from "react";
+import { pdfjs, Document, Page as ReactPdfPage } from "react-pdf";
+import HTMLFlipBook from "react-pageflip";
+import samplePDF from "./sample.pdf";
 import { PageSubSection } from "../common/PageSubSection";
+
+const Page: React.FC<{ pageNumber: number }> = ({ pageNumber }) => {
+    const pageRef = useRef(null);
+
+    return (
+        <div ref={pageRef}>
+            <ReactPdfPage pageNumber={pageNumber} width={300} />
+        </div>
+    );
+};
+
+function PDFFlipBook() {
+    return (
+        <Document file={samplePDF}>
+            <HTMLFlipBook width={500} height={500}>
+                <Page pageNumber={1} />
+                <Page pageNumber={2} />
+                <Page pageNumber={3} />
+            </HTMLFlipBook>
+        </Document>
+    );
+}
 
 export const ImpactReportSection = () => {
     return (
         <PageSubSection title={"Annual Impact Report"}>
             <div className="flex items-center justify-center">
-                <Image
-                    src={annualImpactReportImg}
-                    alt={"Annual impact report"}
-                    className="w-full max-w-2xl sm:w-2/3"
-                />
+                <PDFFlipBook />
             </div>
         </PageSubSection>
     );
