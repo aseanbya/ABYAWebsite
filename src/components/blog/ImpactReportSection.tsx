e8abda9858a0454511ea2305d736062debeb5d54
mochicooki3 <132681043+mochicooki3@users.noreply.github.com>
2023-09-16 16:32:44 +0800

    fix: embedding pdf page as temp fix

--- src/components/blog/ImpactReportSection.tsx
+++ src/components/blog/ImpactReportSection.tsx
@@ -1,32 +1,16 @@
-import React, { useState } from "react";
-import { Document, Page } from "react-pdf";
+import React from "react";
 import { PageSubSection } from "../common/PageSubSection";
 
-function PDFFlipBook() {
-    const [numPages, setNumPages] = useState<number>();
-    const [pageNumber, setPageNumber] = useState<number>(1);
-
-    function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-        setNumPages(numPages);
-    }
-
-    return (
-        <div>
-            <Document file="/sample.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-                <Page pageNumber={pageNumber} />
-            </Document>
-            <p>
-                Page {pageNumber} of {numPages}
-            </p>
-        </div>
-    );
-}
-
-export const ImpactReportSection = () => {
+export const ImpactReportSection: React.FC = () => {
     return (
         <PageSubSection title={"Annual Impact Report"}>
             <div className="flex items-center justify-center">
-                <PDFFlipBook />
+                <embed
+                    src="/sample.pdf"
+                    type="application/pdf"
+                    width="100%"
+                    height="600px"
+                />
             </div>
         </PageSubSection>
     );
