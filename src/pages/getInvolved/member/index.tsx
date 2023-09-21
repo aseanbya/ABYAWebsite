6bf2ed09fdf9dd169c1e2aaf5d98c6d295b65c67
sheng <chengzhisheng2001@gmail.com>
2023-09-21 12:28:03 +0800

    feat: create application form component
    Create an application form using Formik, a React library
    which simplifies the process of creating forms.

--- src/pages/getInvolved/member/index.tsx
+++ src/pages/getInvolved/member/index.tsx
@@ -1,10 +1,13 @@
+import ContentContainer from "~/components/common/ContentContainer";
 import PageLayout from "~/components/common/PageLayout";
-
+import { ApplicationForm } from "~/components/getInvolved/ApplicationForm";
 
 export default function Home() {
     return (
         <PageLayout>
-            getinvolved
+            <ContentContainer>
+                <ApplicationForm />
+            </ContentContainer>
         </PageLayout>
     );
 }
