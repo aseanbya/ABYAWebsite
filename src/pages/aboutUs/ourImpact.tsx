25420947d91527c9ff914577ebe17e72ee1e1f2f
sheng <chengzhisheng2001@gmail.com>
2024-01-05 09:52:31 +0800

    feat: add sample pdf

--- src/pages/aboutUs/ourImpact.tsx
+++ src/pages/aboutUs/ourImpact.tsx
@@ -1,9 +1,19 @@
+import dynamic from "next/dynamic";
 import PageLayout from "~/components/common/PageLayout";
 
+/**
+ * Lazy load the pdf component to avoid SSR issues:
+ * See example from library: https://github.com/wojtekmaj/react-pdf/blob/main/sample/next-pages/pages/index.tsx
+ */
+const Sample = dynamic(() => import("~/components/pdf-sample"), {
+  ssr: false,
+});
+
 export default function OurImpact() {
-    return (
-        <PageLayout>
-            getinvolved
-        </PageLayout>
-    );
+  return (
+    <PageLayout>
+      getinvolved
+      <Sample />
+    </PageLayout>
+  );
 }
