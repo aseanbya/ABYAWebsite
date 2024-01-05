afc63a435cab1443be30b389376a1e2470039616
sheng <chengzhisheng2001@gmail.com>
2024-01-05 10:06:16 +0800

    feat: add pagination

--- src/components/pdf-sample.tsx
+++ src/components/pdf-sample.tsx
@@ -25,6 +25,8 @@ type PDFFile = string | File | null;
 export default function Sample() {
   const [file, setFile] = useState<PDFFile>("/pdfs/report.pdf");
   const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState(1);
+
   const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
   const [containerWidth, setContainerWidth] = useState<number>();
 
@@ -52,31 +54,69 @@ export default function Sample() {
     setNumPages(nextNumPages);
   }
 
+  function changePage(offset: number) {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
+
   return (
     <div className="Example">
       <header>
         <h1>react-pdf sample page</h1>
       </header>
-      <div className="Example__container">
-        <div className="Example__container__load">
-          <label htmlFor="file">Load from file:</label>{" "}
-          <input onChange={onFileChange} type="file" />
+      <div className="mx-2 flex flex-col items-center p-2">
+        {/* FOR TESTING: Should not allow users to choose input file - can remove */}
+        <div className="mt-1 flex flex-col justify-center gap-2">
+          {/* <label htmlFor="file">Load from file:</label>{" "}
+          <input
+            onChange={onFileChange}
+            type="file"
+            className="file-input file-input-bordered file-input-primary w-full max-w-xs"
+          /> */}
+          <h1 className="text-center text-lg font-semibold">
+            Page {pageNumber || (numPages ? 1 : "--")} of {numPages ?? "--"}
+          </h1>
+          <div className="flex gap-2 lg:gap-12">
+            <button
+              disabled={pageNumber <= 1}
+              onClick={previousPage}
+              className="btn"
+            >
+              Prev
+            </button>
+            <button
+              disabled={numPages ? pageNumber >= numPages : false}
+              onClick={nextPage}
+              className="btn"
+            >
+              Next
+            </button>
+          </div>
         </div>
-        <div className="Example__container__document" ref={setContainerRef}>
+        <div
+          className="m-w-[calc(100%-2em)] h-screen w-full"
+          ref={setContainerRef}
+        >
           <Document
             file={file}
             onLoadSuccess={onDocumentLoadSuccess}
             options={options}
+            className="flex flex-col items-center"
           >
-            {Array.from(new Array(numPages), (el, index) => (
-              <Page
-                key={`page_${index + 1}`}
-                pageNumber={index + 1}
-                width={
-                  containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth
-                }
-              />
-            ))}
+            <Page
+              pageNumber={pageNumber}
+              width={
+                containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth
+              }
+              className="m-2 shadow-md"
+            />
           </Document>
         </div>
       </div>
