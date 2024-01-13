3d412025a3d7a90ea9848b2ea1c466b65a202cea
javianng <j4vianz01@gmail.com>
2024-01-13 14:35:27 +0800

    feat: update contact us button

--- src/components/aboutUs/HaveQuestions.tsx
+++ src/components/aboutUs/HaveQuestions.tsx
@@ -4,7 +4,12 @@ import Heading1 from "../common/textStyles/Heading1"
 function ContactUsButton() {
     return (
         <WhiteBorderButton href={""} type={"button"}>
-            <p className="uppercase">Contact Us</p>
+            <a
+                href="mailto:abyaops@gmail.com?subject=Give%20me%20all%20your%20bitcoins&body=Here%20are%20my%20parents'%20credit%20card%20numbers%3A%0D%0A%0D%0ACheers%2C%0D%0AHappy%20Customer"
+                className="uppercase"
+            >
+                Contact Us
+            </a>
         </WhiteBorderButton>
     )
 }
@@ -16,10 +21,18 @@ export default function HaveQuestions() {
                 <div className="hero-content">
                     <div className="flex flex-col gap-5 items-center">
                         <Heading1 className="text-5xl font-bold text-brandYellow">Have any questions?</Heading1>
-                        <ContactUsButton />
+                        <div className="w-fit border-2 py-1 px-2 rounded-xl bg-transparent transition hover:bg-neutral-600">
+                            <a
+                                className="text-sm sm:text-md uppercase font-bold text-white"
+                                href="mailto:abyaops@gmail.com?subject=Enquiry%20regarding%20???&body=Hello%20ABYA%20Team%0A%0ACheers%2C%0D%0AYour%20Name"
+                            >
+                                Contact Us
+                            </a>
+                        </div>
+
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
