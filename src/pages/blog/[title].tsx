1b19cf812770cabd8813b6c2b16b5584203fc193
javianng <j4vianz01@gmail.com>
2024-01-17 12:05:13 +0800

    edit

--- src/pages/blog/[title].tsx
+++ src/pages/blog/[title].tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import PageLayout from '~/components/common/PageLayout';
 import ContentContainer from '~/components/common/ContentContainer';
 import Heading2 from '~/components/common/textStyles/Heading2';
+import Markdown from 'react-markdown';
 // import { Interweave } from 'interweave';
 
 export default function BlogPost() {
@@ -17,7 +18,8 @@ export default function BlogPost() {
                     <Heading2 className='sm:w-[50%] flex items-center'>{data?.title}</Heading2>
                     <Image className='sm:w-[50%]' src={data?.image ?? ""} width={1600} height={1600} alt={data?.title ?? ""} />
                 </div>
-                <p>{data?.content}</p>
+                {/* <p>{data?.content}</p> */}
+                <Markdown>{data?.content}</Markdown>
             </ContentContainer>
         </PageLayout >
     )
