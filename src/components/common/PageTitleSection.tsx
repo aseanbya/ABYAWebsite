af82ec2826c3493e528a92dd62fbbc0db609e319
Javian Ng <j4vianz01@gmail.com>
2024-03-12 03:12:07 +0800

    feat: implement pagetitle section

--- src/components/common/PageTitleSection.tsx
+++ src/components/common/PageTitleSection.tsx
@@ -6,20 +6,22 @@ export default function PageTitleSection({
   children,
 }: PageTitleSectionProps) {
   return (
-    <div className="w-full max-w-7xl items-center">
-      <div>
-        <h1 className="pb-4 uppercase">{title}</h1>
-        {children}
-      </div>
-      <div className="flex justify-between">
-        <div className="w-0 sm:w-1/3"></div>
-        <Image
-          src={"/singaporeAttractionsGrey.png"}
-          alt={""}
-          width={400}
-          height={400}
-          className="w-full max-w-2xl sm:w-2/3"
-        />
+    <div className="flex items-center justify-center py-12 pt-24">
+      <div className="relative w-full max-w-7xl">
+        <div className="absolute">
+          <h1 className="pb-4 text-6xl font-bold uppercase">{title}</h1>
+          {children}
+        </div>
+        <div className="flex justify-between">
+          <div className="w-0 sm:w-1/3"></div>
+          <Image
+            src={"/singaporeAttractionsGrey.png"}
+            alt={""}
+            width={400}
+            height={400}
+            className="w-full max-w-2xl sm:w-2/3"
+          />
+        </div>
       </div>
     </div>
   );
--- src/pages/aboutUs/index.tsx
+++ src/pages/aboutUs/index.tsx
@@ -4,7 +4,12 @@ import PageTitleSection from "~/components/common/PageTitleSection";
 export default function AboutUs() {
   return (
     <PageLayout>
-      <PageTitleSection title={"asdasd"} children={"sadeasdd"} />
+      <PageTitleSection
+        title={"asdasd"}
+        children={
+          "Excepteur id cupidatat esse adipisicing reprehenderit veniam occaecat amet fugiat id sit fugiat. Est elit excepteur commodo magna deserunt anim nisi irure. Elit magna non quis ipsum adipisicing consectetur et excepteur. Nulla in elit amet exercitation officia ea fugiat sint. Incididunt dolore irure consequat consequat amet minim aliqua excepteur. Deserunt deserunt proident ipsum non cupidatat ex et laborum laboris dolor id excepteur ea tempor nulla. Ullamco magna laborum labore aliqua incididunt cillum. Excepteur id cupidatat esse adipisicing reprehenderit veniam occaecat amet fugiat id sit fugiat. Est elit excepteur commodo magna deserunt anim nisi irure. Elit magna non quis ipsum adipisicing consectetur et excepteur. Nulla in elit amet exercitation officia ea fugiat sint. Incididunt dolore irure consequat consequat amet minim aliqua excepteur. Deserunt deserunt proident ipsum non cupidatat ex et laborum laboris dolor id excepteur ea tempor nulla. Ullamco magna laborum labore aliqua incididunt cillum."
+        }
+      />
     </PageLayout>
   );
 }
