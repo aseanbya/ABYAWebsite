ae82a552fad2cd6e5385a048d1727f093967041a
Javian Ng <j4vianz01@gmail.com>
2024-03-12 14:33:21 +0800

    fix: lint

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -110,8 +110,8 @@ export const ProductItem = ({
   );
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const HoveredLink = ({ children, ...rest }: any) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return (
     <Link
       {...rest}
